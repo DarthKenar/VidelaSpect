ad948dab2f76b89b35da25206ae43e436c4d078b
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-18 20:33:32 -0300

    📦 Configuration:  Config tailwinds & static files

--- src/build/app.ts
+++ src/build/app.ts
@@ -1,9 +1,9 @@
 import "reflect-metadata"
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
-const app = express()
-
 import { engine } from 'express-handlebars';
+const app = express()
+const PATH = require("path")
 
 //Handlebars config
 app.engine('handlebars', engine());
@@ -11,7 +11,12 @@ app.set('view engine', 'handlebars');
 app.set('views', './dist/views');
 //
 
+
 //Middlewares
+
+app.use(express.static(PATH.join(__dirname, '../public')));
+//
+
 app.use((req:Request, res:Response, next:NextFunction)=>{
     if(req.method !== "GET" && req.method !== "DELETE" && req.method !== "POST"){
         res.status(501).json({error:"El método solicitado no está soportado por el servidor y no puede ser manejado."})
@@ -30,7 +35,7 @@ app.use((req:Request, res:Response)=>{
     console.log(req.method)
     console.log(req.body)
     console.log(req.path)
-    res.status(404).json({error:"La pagina solicitada no se encuentra."})
+    res.status(404).json(console.log(PATH.join(__dirname, '../public')))
 })
 
 export default app;
