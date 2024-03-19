0149d7048048e3aeeb594503e58d71c7ed03d1c0
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-19 18:31:39 -0300

    ✨ New Feature: Se agregaron algunas rutas para manejar los errores del servidor, y se agrego la ruta postRegistroDNI sin probar que maneja la solicitud post de un DNI

--- src/build/controllers/personal.controller.ts
+++ src/build/controllers/personal.controller.ts
@@ -1,9 +1,43 @@
 import { Request, Response } from "express";
+import DataBase from "../../database/data-source";
+import { Personal } from "../../database/entity/models";
 
 export const getRegistroDNI = async (req:Request, res:Response)=>{
     res.render("registroDNI")
 }
+
 export const getRegistroFoto = async (req:Request, res:Response)=>{
     res.render("registroFoto")
 }
 
+export const postRegistroDNI = async (req:Request, res:Response)=>{
+    let error:string[] = []
+    try{
+        if(req.query.dni){
+            let dni:string = req.query.dni.toString()
+            let personalRepository = DataBase.getRepository(Personal)
+            let persona = await personalRepository.findOneBy({dni})
+            if(persona){
+                res.render("/personal/foto", persona)
+            }else{
+                error.push(`El número de DNI - ${dni} no está registrado en el sistema. Contacte al administrador.`)
+                res.render("/personal/dni",error)
+            }
+        }else{
+            error.push("El número de DNI no fue ingresado.")
+            res.render("/personal/dni",error)
+        }
+    }catch(err){
+        console.log(err)
+        res.redirect("/personal/500")
+    }
+}
+
+export const postRegistroFoto = async (req:Request, res:Response)=>{
+    res.redirect("/personal/dni")
+}
+
+
+export const get500 = async (req:Request, res:Response)=>{
+    res.render("500")
+}
--- src/build/routes/personal.routes.ts
+++ src/build/routes/personal.routes.ts
@@ -3,11 +3,21 @@ const routerPersonal = express.Router();
 
 import {
   getRegistroDNI,
-  getRegistroFoto
+  getRegistroFoto,
+  postRegistroDNI,
+  postRegistroFoto,
+  get500
 } from "../controllers/personal.controller"
 
+//GET
 routerPersonal.get("/dni", getRegistroDNI)
 routerPersonal.get("/foto", getRegistroFoto)
+//POST
+routerPersonal.get("/dni/send", postRegistroDNI)
+routerPersonal.get("/foto/send", postRegistroFoto)
+
+//ERRORES
+routerPersonal.get("/500", get500)
 
 module.exports = routerPersonal;
 
