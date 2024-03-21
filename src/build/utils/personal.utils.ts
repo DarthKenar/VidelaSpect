1f5927f829358bf43fe9924a498b21e678547c4b
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-21 15:16:16 -0300

    👌 IMPROVE: Ahora las imágenes se guardan correctamente

--- src/build/utils/personal.utils.ts
+++ src/build/utils/personal.utils.ts
@@ -13,13 +13,26 @@ export interface Image {
 
 export function saveImage(personal:Personal, image:Image|undefined){
     if(image){
-        fs.writeFile(`${personal.name}`+".png", image.buffer, function(err:Error) {
-            if (err) {
-              console.log('Hubo un error al escribir el archivo', err);
-            } else {
-              console.log('Archivo guardado con éxito');
-            }
-          });
+      let fecha = new Date
+      let mes = fecha.getMonth()
+      let dia = fecha.getDate()
+      let horas = fecha.getHours()
+      let minutos = fecha.getMinutes()
+      fs.writeFile(`dist/database/fotos/${dia}-${mes}/${horas}.${minutos}-${personal.name}`+".png", image.buffer, function(err:Error) {
+          if (err) {
+            console.log('Hubo un error al escribir el archivo', err);
+            fs.mkdirSync(`./dist/database/fotos/${dia}-${mes}/`,{recursive:true});
+            fs.writeFile(`dist/database/fotos/${dia}-${mes}/${horas}.${minutos}-${personal.name}`+".png", image.buffer,function(err:Error) {
+              if(err){
+                console.log(err)
+              }else{
+                console.log("La carpeta se ha creado correctamente")
+              }
+            })
+          } else {
+            console.log('Archivo guardado con éxito');
+          }
+        });
     }else{
         console.log("La imagen no se está enviando correctamente.")
     }
