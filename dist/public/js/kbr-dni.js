f321c32fd82722db9375b22351f9b1fef005370d
Federico Vega PC <federico.vega2222@gmail.com>
2024-03-21 17:42:02 -0300

    💄 UI:  Ahora el reloj se muestra correctamente y se agregó un botón para borrar los numeros del DNI en al interfaz

--- dist/public/js/kbr-dni.js
+++ dist/public/js/kbr-dni.js
@@ -6,5 +6,19 @@ teclado.addEventListener('click', function(event) {
     if(!isNaN(numero)){
         dniLabel.value = `${dniLabel.value}${numero}`
     }
+    if(numero === "backspace"){
+        dniLabel.value = dniLabel.value.slice(0, -1);
+    }
 });
 
+
+
+window.onload = function() {
+    setInterval(function() {
+        let fecha = new Date();
+        document.getElementById('dia').style.setProperty('--value', fecha.getDate());
+        document.getElementById('hora').style.setProperty('--value', fecha.getHours());
+        document.getElementById('minutos').style.setProperty('--value', fecha.getMinutes());
+        document.getElementById('segundos').style.setProperty('--value', fecha.getSeconds());
+    }, 1000);
+};
