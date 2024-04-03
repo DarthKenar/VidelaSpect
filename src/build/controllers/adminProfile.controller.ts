5c9b5317da0ee2ffb7b2710afff9f53606add9c5
Federico Vega PC <federico.vega2222@gmail.com>
2024-04-03 00:56:56 -0300

    ✨ New Feature: Ahora se puede actualizar el email´del admin 🚧 Work in Progress: - Sección actualizar contraseña - Validadores para email - Catch errores y devolver al usuario una respuesta cuando no existe el email para enviar. (combinar con validadores de creación de email [usar los mismos]) - Agregar paginación en personal y registros

--- src/build/controllers/adminProfile.controller.ts
+++ src/build/controllers/adminProfile.controller.ts
@@ -1,14 +1,79 @@
-import { Request, Response } from "express";
-import { getPersonalWhitId } from "../utils/adminPanel.utils";
+import e, { Request, Response } from "express";
+import { getAuth, getPersonalWhitId } from "../utils/adminPanel.utils";
 import { ValidationClass } from "../interfaces/interfaces";
+import DataBase from "../../database/data-source";
+import { passwordValidations } from "../validators/personal.validator";
+
 export const getProfile = async (req:Request, res:Response) => {
     let personal = await getPersonalWhitId(req.cookies.userId)
     if (personal) {
-        res.render("adminOptions", {personal})
+        res.render("adminProfile", {personal})
     }else{
         let validation = new ValidationClass
         validation.addMessage("No se encontró el usuario, por favor inicie sesión nuevamente.", "error")
         res.render("error", {messages: validation.messages})
     }
-    
+}
+
+export const getProfileEmail = async (req:Request, res:Response) => {
+    let personal = await getPersonalWhitId(req.cookies.userId)
+    if (personal) {
+        let auth = await getAuth(personal)
+        res.render("adminProfileEmail", {personal, email: auth.email})
+    }else{
+        let validation = new ValidationClass
+        validation.addMessage("No se encontró el usuario, por favor inicie sesión nuevamente.", "error")
+        res.render("error", {messages: validation.messages})
+    }
+}
+
+export const getProfilePassword = async (req:Request, res:Response) => {
+    let personal = await getPersonalWhitId(req.cookies.userId)
+    if (personal) {
+        res.render("adminProfilePassword", {personal})
+    }else{
+        let validation = new ValidationClass
+        validation.addMessage("No se encontró el usuario, por favor inicie sesión nuevamente.", "error")
+        res.render("error", {messages: validation.messages})
+    }
+}
+
+export const postProfileEmail = async (req:Request, res:Response) => {
+    let personal = await getPersonalWhitId(req.cookies.userId)
+    let email = req.body.email
+    let validation = new ValidationClass
+    if (personal) {
+        let auth = await getAuth(personal)
+        if (auth) {
+            auth.email = email
+            DataBase.manager.save(auth)
+            validation.addMessage("Email actualizado correctamente.", "success")
+            res.render("adminProfileEmail", {personal, email, messages: validation.messages})
+        }
+    }else{
+        validation.addMessage("No se encontró el usuario, por favor inicie sesión nuevamente.", "error")
+        res.render("error", {messages: validation.messages})
+    }
+}
+
+export const postProfilePassword = async (req:Request, res:Response) => {
+    let personal = await getPersonalWhitId(req.cookies.userId)
+    let password = req.body.password
+    let password2 = req.body.password2
+    let validation = new ValidationClass
+    if (personal) {
+        let auth = await getAuth(personal)
+        if (auth) {
+            validation = passwordValidations(validation, password, password2)
+            if (validation.status) {
+                auth.password = password
+                DataBase.manager.save(auth)
+                validation.addMessage("Contraseña actualizada correctamente.", "success")
+                res.render("adminProfilePassword", {personal, messages: validation.messages})
+            }
+        }
+    }else{
+        validation.addMessage("No se encontró el usuario, por favor inicie sesión nuevamente.", "error")
+        res.render("error", {messages: validation.messages})
+    }
 }
--- src/build/routes/adminProfile.routes.ts
+++ src/build/routes/adminProfile.routes.ts
@@ -3,8 +3,19 @@ const routerProfile = express.Router();
 
 import {
     getProfile,
+    getProfileEmail,
+    getProfilePassword,
+    postProfileEmail,
+    postProfilePassword
 } from "../controllers/adminProfile.controller"
 
+//GET
 routerProfile.get("", getProfile)
+routerProfile.get("/email", getProfileEmail)
+routerProfile.get("/password", getProfilePassword)
+
+//POST
+routerProfile.post("/email/send", postProfileEmail)
+routerProfile.post("/password/send", postProfilePassword)
 
 module.exports = routerProfile;
