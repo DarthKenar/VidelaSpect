e1e1967320be3260f9beea4b628759e58ae9908a
Federico Vega PC <federico.vega2222@gmail.com>
2024-04-04 15:18:36 -0300

    🥅 Try-catch-fix:  token expirado

--- src/build/middlewares/verifyToken.middleware.ts
+++ src/build/middlewares/verifyToken.middleware.ts
@@ -5,13 +5,25 @@ import { ValidationClass } from "../interfaces/interfaces";
 
 
 export function verifyToken(req:Request, res:Response, next:NextFunction){
-    const token = req.cookies.token
-    if(!token){
+    try{
+        const token = req.cookies.token
+        if(!token){
+            let validation = new ValidationClass
+            validation.addMessage("Usted no tiene permisos de administrador para acceder a esta sección.", "error")
+            return res.render("error", {messages: validation.messages})
+        }else{
+            const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
+            next()
+        }
+    }catch (error) {
         let validation = new ValidationClass
-        validation.addMessage("Usted no tiene permisos de administrador para acceder a esta sección.", "error")
-        return res.render("error", {messages: validation.messages})
-    }else{
-        const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
-        next()
+        if (error instanceof jwt.TokenExpiredError) {
+            validation.addMessage("La sesión ha expirado, por favor vuelva a iniciar sesión", "error")
+            return res.render("error", {messages: validation.messages})
+        } else {
+            validation.addMessage("Ha ocurrido un error inesperado en la sesión. Por favor vuelva a ingresar.", "error")
+            return res.render("error", {messages: validation.messages})
+        }
     }
+
 }
