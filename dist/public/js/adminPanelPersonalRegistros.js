7afcf3d7c4c3587ec1fc6a219bcb95a22d29c271
Federico Vega PC <federico.vega2222@gmail.com>
2024-04-04 14:24:03 -0300

    👌 IMPROVE: Ahora No se le permite al usuario solicitar repetidamente envío de correos. por el contrario, el botón que realiza la request se modifica a un spinner de carga y cuando la solicitud se completa, devuelvo el botón para que realice una nueva petición si así el usuario quiere.

--- dist/public/js/adminPanelPersonalRegistros.js
+++ dist/public/js/adminPanelPersonalRegistros.js
@@ -1,11 +1,13 @@
-
-function enableButton(){
-    let downloadOrSendBtn = document.getElementById("loadingBtn")
-    downloadOrSendBtn.outerHTML = `<a hx-get="/admin/panel/personal/excel" hx-trigger="click" hx-target="#search_results" hx-include="[name='select'], [name='input'], [name='email']" class="btn btn-outline btn-primary btn-sm" title="Descargar/Enviar Excel de Personal" id="downloadOrSendBtn" onclick="disabledButton()">SOLICITAR</a>`
+function enableButton(target){
+    console.log("enableButton")
+    var downloadOrSendBtn = document.getElementById("loadingBtn")
+    downloadOrSendBtn.outerHTML = `<a hx-get="/admin/panel/${target}/excel" hx-trigger="click" hx-target="#search_results" hx-include="[name='select'], [name='input'], [name='email']" class="btn btn-outline btn-primary btn-sm" title="Descargar/Enviar Excel de ${target}" id="downloadOrSendBtn" hx-on::before-request="disabledButton()" hx-on::after-request="enableButton('${target}')">SOLICITAR</a>`
+    htmx.process(document.getElementById('downloadOrSendBtn'));
 }
-enableButton()
+
 function disabledButton(){
-    let downloadOrSendBtn = document.getElementById('downloadOrSendBtn')
-    downloadOrSendBtn.outerHTML = '<button class="btn" id="loadingBtn"><span class="loading loading-spinner"></span>Espere</button>'
+    console.log("disabledButton")
+    var downloadOrSendBtn = document.getElementById('downloadOrSendBtn')
+    downloadOrSendBtn.outerHTML = '<button class="btn btn-sm" id="loadingBtn"><span class="loading loading-spinner"></span>Espere</button>'
 }
 
